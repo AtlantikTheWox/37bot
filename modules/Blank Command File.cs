f9f67735dca5fab01be92308c11667ce6d6e93ab
HappyLP <HappyLP@YEETBOX>
2021-01-22 22:39:04 +0100

    Small changes

---
+++ modules/Blank Command File.cs
@@ -0,0 +1,20 @@
+﻿using Discord;
+using Discord.Net;
+using Discord.WebSocket;
+using Discord.Commands;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using botof37s;
+namespace botof37s.Modules
+{
+
+    
+    public class Example : ModuleBase
+    {
+
+    }
+}
